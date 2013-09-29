d16b493ae5a6bc2927193ec9caee7123c23a8c92
mgoszcz2 <mgoszcz2@gmail.com>
2013-09-29 14:47:26 +0200

    Revamped nav
    * More comments in the css
    * Js removed

--- js/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function(data){
-  var nav = document.getElementsByTagName('nav')[0];
-  if (document.body.scrollTop) {
-    nav.className = "scrolled";
-  } else {
-    nav.className = "";
-  }
-});
